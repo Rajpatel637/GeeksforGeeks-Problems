add314e21123ec16a05e0e4b1087452452242a91
PATEL RAJ PRAVINKUMAR <150596387+Rajpatel637@users.noreply.github.com>
2026-09-21 18:54:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Deletions for Palindrome/minimum-deletions-for-palindrome.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    private:
+    
+       int lcs(string &s1,string &s2){
+           int l1 = s1.length();
+           int l2 = s2.length();
+           
+           vector<vector<int>> ans(l1+1,vector<int>(l2+1));
+           
+           
+           for(int i = 0; i < l1+1;i++){
+               for(int j = 0; j < l2+1;j++){
+                   if( i == 0 || j == 0){
+                       ans[i][j] = 0;
+                   }
+               }
+           }
+           
+           for(int i = 1; i < l1+1;i++){
+               for(int j = 1; j < l2+1;j++){
+                   if(s1[i-1] == s2[j-1]){
+                       ans[i][j] = 1 + ans[i-1][j-1];
+                   }
+                   else{
+                       ans[i][j] = max(ans[i][j-1],ans[i-1][j]);
+                   }
+               }
+           }
+           
+           return ans[l1][l2];
+       }
+    
+    
+  public:
+    int minDeletions(string &s) {
+        
+        int l1 = s.length();
+        
+        string revStr = s;
+        reverse(revStr.begin(),revStr.end());
+        
+        return l1 - lcs(s,revStr);
+        
+    }
+};
